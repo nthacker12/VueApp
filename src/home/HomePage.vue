c64148f20e6685d7c7e0cef43b53299cfa16050f
Nick Thacker <nthacker@riskcontroltech.com>
2019-01-10 17:58:41 -0500

    Create new component, add images, check rendering before routing.

--- src/home/HomePage.vue
+++ src/home/HomePage.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="home">
     <div>
-      <img class="robot" src="../assets/robot-home.png" aria-hidden="true" />
+      <img class="robot" src="../assets/robot-home.png" aria-hidden="true">
     </div>
     <div>
       <div class="get-started">
-        <a href="">Get Started</a> building your first robot!
+        <a href>Get Started</a> building your first robot!
       </div>
     </div>
   </div>
@@ -22,18 +22,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.home {
+  text-align: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.robot {
+  height: 300px;
 }
 </style>
